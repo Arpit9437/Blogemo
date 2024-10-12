07987cdd19446020026085679b8793a132b7fccc
Arpit9437 <137411512+Arpit9437@users.noreply.github.com>
2024-10-12 14:16:19 +0530

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000/blog/api";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/blog/api" : "/blog/api";
 
 export const useAuthStore = create((set) => ({
 	user: null,
